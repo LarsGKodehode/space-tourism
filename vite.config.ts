af77c578dd7f805256b6e21cc2b81198ce2f9b5f
LarsG <lars.ga.solheim@gmail.com>
2023-01-28 00:32:27 +0100

    moved to vite plugin for resolving paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,11 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import tsconfigpaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
+  plugins: [react(), tsconfigpaths()],
 
   // Setting up the tests
   test: {
