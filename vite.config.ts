35685b89f8596e16405285e345eb381ad1a4e639
LarsG <lars.ga.solheim@gmail.com>
2023-01-28 00:06:32 +0100

    config added absolute path resolvers to Vite and TS

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: [
+      {find: "@", replacement: path.resolve(__dirname, 'src')}
+    ]
+  },
+
+  // Setting up the tests
   test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./setupTests.ts'],
