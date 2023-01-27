999246cc2757f30e4d3ea2afabd7aca586abc515
LarsG <lars.ga.solheim@gmail.com>
2023-01-28 00:13:08 +0100

    config vite replaced old require with new import

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
